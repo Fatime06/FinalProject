59437646e9e924dc6ba35b5da84da55555393d5d
Fatime06 <fatimafa@code.edu.az>
2025-11-26 02:31:17 +0400

    added Layers

---
+++ Final/Repository/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
