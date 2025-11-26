9b60676146faaccc3629a739dcbf6b3688df4daa
Fatime06 <fatimafa@code.edu.az>
2025-11-27 02:09:06 +0400

    finished ViewComponent

--- Final/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ Final/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e84c122f099ecfd1285d0e59d994ec5baf144f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179735cccd11eb188a09c4d3e25a2ceb8b3a2c1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
